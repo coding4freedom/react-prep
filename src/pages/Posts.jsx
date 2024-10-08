ff99457169a2bc69505c071bea15d97f6772f24f
coding4freedom <chawkins2010@hotmail.com>
2024-10-07 22:52:42 -0500

    User card with api call added, route to home page added

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const Posts = () => {
@@ -28,7 +28,9 @@ const Posts = () => {
   return (
     <>
       <div className="post__search">
-        <button>← Back</button>
+        <Link to="/">
+          <button>← Back</button>        
+        </Link>
         <div className="post__search--container">
           <label className="post__search--label">Search by Id</label>
           <input 
